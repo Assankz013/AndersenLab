5364c4ee0ef06addba23dcd91219c882dc2159de
Assan <assan.tildabay013kz@gmail.com>
2022-05-23 22:26:19 +0600

    Lesson_2

--- AndersenLabTasks/src/main/java/com/lesson_2/Task_2.java
+++ AndersenLabTasks/src/main/java/com/lesson_2/Task_2.java
@@ -1,6 +1,15 @@
 package com.lesson_2;
 
+import java.util.Scanner;
+
 public class Task_2 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        printSignOfNumber(x);
+    }
+
     public static void printSignOfNumber(int a){
         System.out.println((a >= 0) ? "Положительный" : "Отрицательный");
     }
--- AndersenLabTasks/src/main/java/com/lesson_2/Task_4.java
+++ AndersenLabTasks/src/main/java/com/lesson_2/Task_4.java
@@ -1,6 +1,16 @@
 package com.lesson_2;
 
+import java.util.Scanner;
+
 public class Task_4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String str = in.next();
+        int n = in.nextInt();
+        printString(str,n);
+        int year = in.nextInt();
+        System.out.println(isLeapYear(year));
+    }
     public static void printString(String str,int n){
         for (int i = 0;i<n;i++){
             System.out.println(str);
